propagate correct local efficiency calculation to efficiency_bin

bct/bct/algorithms/distance.py
@@ -374,7 +374,7 @@ def efficiency_bin(G,local=False):
 
             #symmetrized adjacency vector
             sa=G[u,V]+G[V,u].T
-            numer = np.sum(np.dot(sa.T,sa)*se)/2
+            numer = np.sum(np.outer(sa.T,sa)*se)/2
             if numer!=0:
                 denom = np.sum(sa)**2 - np.sum(sa*sa)
                 E[u] = numer/denom		#local efficiency
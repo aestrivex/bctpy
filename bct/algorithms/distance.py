f7c434d125de9720a4cafdf1c9f3e72974c51bd9
Ross Markello <rossmarkello@gmail.com>
2017-10-13 08:54:36 -0400

    NF: Added new functionality to `distance.py`
    New functions include:
    * `mean_first_passage_time()`
    * `search_information()`
    * `distance_wei_floyd()`
    * `retrieve_shortest_path()`
    
    The latter two functions are helper functions for `search_information()`.
    Functions are tested against several, but not an exhaustive number of cases;
    well-thought tests may be warranted.
    
    Also updated `CREDITS` with relevant information.

--- bct/algorithms/distance.py
+++ bct/algorithms/distance.py
@@ -323,6 +323,149 @@ def distance_wei(G):
     return D, B
 
 
+def distance_wei_floyd(adjacency, transform=None):
+    """
+    Computes the topological length of the shortest possible path connecting
+    every pair of nodes in the network.
+
+    Parameters
+    ----------
+    D : (N x N) array_like
+        Weighted/unweighted, direct/undirected connection weight/length array
+    transform : str, optional
+        If `adjacency` is a connection weight array, specify a transform to map
+        input connection weights to connection lengths. Options include ['log',
+        'inv'], where 'log' is `-np.log(adjacency)` and 'inv' is `1/adjacency`.
+        Default: None
+
+    Returns
+    -------
+    SPL : (N x N) ndarray
+        Weighted/unweighted shortest path-length array. If `D` is a directed
+        graph, then `SPL` is not symmetric
+    hops : (N x N) ndarray
+        Number of edges in the shortest path array. If `D` is unweighted, `SPL`
+        and `hops` are identical.
+    Pmat : (N x N) ndarray
+        Element `[i,j]` of this array indicates the next node in the shortest
+        path between `i` and `j`. This array is used as an input argument for
+        function `retrieve_shortest_path()`, which returns as output the
+        sequence of nodes comprising the shortest path between a given pair of
+        nodes.
+
+    Notes
+    -----
+    There may be more than one shortest path between any pair of nodes in the
+    network. Non-unique shortest paths are termed shortest path degeneracies
+    and are most likely to occur in unweighted networks. When the shortest-path
+    is degenerate, the elements of `Pmat` correspond to the first shortest path
+    discovered by the algorithm.
+
+    The input array may be either a connection weight or length array. The
+    connection length array is typically obtained with a mapping from weight to
+    length, such that higher weights are mapped to shorter lengths (see
+    argument `transform`, above).
+
+    Originally written in Matlab by Andrea Avena-Koenigsberger (IU, 2012)
+
+    References
+    ----------
+    .. [1] Floyd, R. W. (1962). Algorithm 97: shortest path. Communications of
+       the ACM, 5(6), 345.
+    .. [2] Roy, B. (1959). Transitivite et connexite. Comptes Rendus
+       Hebdomadaires Des Seances De L Academie Des Sciences, 249(2), 216-218.
+    .. [3] Warshall, S. (1962). A theorem on boolean matrices. Journal of the
+       ACM (JACM), 9(1), 11-12.
+    .. [4] https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm
+    """
+
+    if transform is not None:
+        if transform == 'log':
+            if np.logical_or(adjacency > 1, adjacency < 0).any():
+                raise ValueError("Connection strengths must be in the " +
+                                 "interval [0,1) to use the transform " +
+                                 "-log(w_ij).")
+            SPL = -np.log(adjacency)
+        elif transform == 'inv':
+            SPL = 1. / adjacency
+        else:
+            raise ValueError("Unexpected transform type. Only 'log' and " +
+                             "'inv' are accepted")
+    else:
+        SPL = adjacency.copy().astype('float')
+        SPL[SPL == 0] = np.inf
+
+    n = adjacency.shape[1]
+
+    flag_find_paths = True
+    hops = np.array(adjacency != 0).astype('float')
+    Pmat = np.repeat(np.atleast_2d(np.arange(0, n)), n, 0)
+
+    for k in range(n):
+        i2k_k2j = np.repeat(SPL[:, [k]], n, 1) + np.repeat(SPL[[k], :], n, 0)
+
+        if flag_find_paths:
+            path = SPL > i2k_k2j
+            i, j = np.where(path)
+            hops[path] = hops[i, k] + hops[k, j]
+            Pmat[path] = Pmat[i, k]
+
+        SPL = np.min(np.stack([SPL, i2k_k2j], 2), 2)
+
+    I = np.eye(n) > 0
+    SPL[I] = 0
+
+    if flag_find_paths:
+        hops[I], Pmat[I] = 0, 0
+
+    return SPL, hops, Pmat
+
+
+def retrieve_shortest_path(s, t, hops, Pmat):
+    """
+    Returns nodes comprising shortest path between `s` and `t`
+
+    This function finds the sequence of nodes that comprise the shortest path
+    between a given source and target node.
+
+    Parameters
+    ----------
+    s : int
+        Source node, i.e. node where the shortest path begins
+    t : int
+        Target node, i.e. node where the shortest path ends
+    hops : (N x N) array_like
+        Number of edges in the path. This array may be obtained as the
+        second output argument of the function `distance_wei_floyd`.
+    Pmat : (N x N) array_like
+        Array whose elements `Pmat[k,t]` indicate the next node in the shortest
+        path between nodes `k` and `t`. This array may be obtained as the third
+        output of the function `distance_wei_floyd`.
+
+    Returns
+    -------
+    path : ndarray
+        Nodes (indices) comprising the shortest path between `s` and `t`
+
+    Notes
+    -----
+    Originally written in Matlab by Andrea Avena-Koenigsberger and Joaquin Goni
+    (IU, 2012)
+    """
+
+    path_length = hops[s, t]
+    if path_length != 0:
+        path = np.zeros((int(path_length + 1), 1), dtype='int')
+        path[0] = s
+        for ind in range(1, len(path)):
+            s = Pmat[s, t]
+            path[ind] = s
+    else:
+        path = []
+
+    return path
+
+
 def efficiency_bin(G, local=False):
     '''
     The global efficiency is the average of inverse shortest path length,
@@ -705,8 +848,8 @@ def reachdist(CIJ, ensure_binary=True):
     CIJ : NxN np.ndarray
         binary directed/undirected connection matrix
     ensure_binary : bool
-        Binarizes input. Defaults to true. No user who is not testing 
-        something will ever want to not use this, use distance_wei instead for 
+        Binarizes input. Defaults to true. No user who is not testing
+        something will ever want to not use this, use distance_wei instead for
         unweighted matrices.
 
     Returns
@@ -762,3 +905,153 @@ def reachdist(CIJ, ensure_binary=True):
     D[od0, :] = np.inf
 
     return R, D
+
+
+def search_information(adjacency, transform=None, has_memory=False):
+    """
+    Calculates search information of `adjacency`
+
+    Computes the amount of information (measured in bits) that a random walker
+    needs to follow the shortest path between a given pair of nodes.
+
+    Parameters
+    ----------
+    adjacency : (N x N) array_like
+        Weighted/unweighted, direct/undirected connection weight/length array
+    transform : str, optional
+        If `adjacency` is a connection weight array, specify a transform to map
+        input connection weights to connection lengths. Options include ['log',
+        'inv'], where 'log' is `-np.log(adjacency)` and 'inv' is `1/adjacency`.
+        Default: None
+    has_memory : bool, optional
+        This flag defines whether or not the random walker "remembers" its
+        previous step, which has the effect of reducing the amount of
+        information needed to find the next state. Default: False
+
+    Returns
+    -------
+    SI : (N x N) ndarray
+        Pair-wise search information array. Note that `SI[i,j]` may be
+        different from `SI[j,i]``; hence, `SI` is not a symmetric matrix even
+        when `adjacency` is symmetric.
+
+    References
+    ----------
+    .. [1] Goni, J., van den Heuvel, M. P., Avena-Koenigsberger, A., de
+       Mendizabal, N. V., Betzel, R. F., Griffa, A., Hagmann, P.,
+       Corominas-Murtra, B., Thiran, J-P., & Sporns, O. (2014). Resting-brain
+       functional connectivity predicted by analytic measures of network
+       communication. Proceedings of the National Academy of Sciences, 111(2),
+       833-838.
+    .. [2] Rosvall, M., Trusina, A., Minnhagen, P., & Sneppen, K. (2005).
+       Networks and cities: An information perspective. Physical Review
+       Letters, 94(2), 028701.
+    """
+
+    N = len(adjacency)
+
+    if np.allclose(adjacency, adjacency.T):
+        flag_triu = True
+    else:
+        flag_triu = False
+
+    T = np.linalg.solve(np.diag(np.sum(adjacency, axis=1)), adjacency)
+    _, hops, Pmat = distance_wei_floyd(adjacency, transform)
+
+    SI = np.zeros((N, N))
+    SI[np.eye(N) > 0] = np.nan
+
+    for i in range(N):
+        for j in range(N):
+            if (j > i and flag_triu) or (not flag_triu and i != j):
+                path = retrieve_shortest_path(i, j, hops, Pmat)
+                lp = len(path) - 1
+                if flag_triu:
+                    if np.any(path):
+                        pr_step_ff = np.zeros(lp)
+                        pr_step_bk = np.zeros(lp)
+                        if has_memory:
+                            pr_step_ff[0] = T[path[0], path[1]]
+                            pr_step_bk[lp-1] = T[path[lp], path[lp-1]]
+                            for z in range(1, lp):
+                                pr_step_ff[z] = T[path[z], path[z+1]] / (1 - T[path[z-1], path[z]])
+                                pr_step_bk[lp-z-1] = T[path[lp-z], path[lp-z-1]] / (1 - T[path[lp-z+1], path[lp-z]])
+                        else:
+                            for z in range(lp):
+                                pr_step_ff[z] = T[path[z], path[z+1]]
+                                pr_step_bk[z] = T[path[z+1], path[z]]
+
+                        prob_sp_ff = np.prod(pr_step_ff)
+                        prob_sp_bk = np.prod(pr_step_bk)
+                        SI[i, j] = -np.log2(prob_sp_ff)
+                        SI[j, i] = -np.log2(prob_sp_bk)
+                else:
+                    if np.any(path):
+                        pr_step_ff = np.zeros(lp)
+                        if has_memory:
+                            pr_step_ff[0] = T[path[0], path[1]]
+                            for z in range(1, lp):
+                                pr_step_ff[z] = T[path[z], path[z+1]] / (1 - T[path[z-1], path[z]])
+                        else:
+                            for z in range(lp):
+                                pr_step_ff[z] = T[path[z], path[z+1]]
+
+                        prob_sp_ff = np.prod(pr_step_ff)
+                        SI[i, j] = -np.log2(prob_sp_ff)
+                    else:
+                        SI[i, j] = np.inf
+
+    return SI
+
+
+def mean_first_passage_time(adjacency):
+    """
+    Calculates mean first passage time of `adjacency`
+
+    The first passage time from i to j is the expected number of steps it takes
+    a random walker starting at node i to arrive for the first time at node j.
+    The mean first passage time is not a symmetric measure: `mfpt(i,j)` may be
+    different from `mfpt(j,i)`.
+
+    Parameters
+    ----------
+    adjacency : (N x N) array_like
+        Weighted/unweighted, direct/undirected connection weight/length array
+
+    Returns
+    -------
+    MFPT : (N x N) ndarray
+        Pairwise mean first passage time array
+
+    References
+    ----------
+    .. [1] Goni, J., Avena-Koenigsberger, A., de Mendizabal, N. V., van den
+       Heuvel, M. P., Betzel, R. F., & Sporns, O. (2013). Exploring the
+       morphospace of communication efficiency in complex networks. PLoS One,
+       8(3), e58070.
+    """
+
+    P = np.linalg.solve(np.diag(np.sum(adjacency, axis=1)), adjacency)
+
+    n = len(P)
+    D, V = np.linalg.eig(P.T)
+
+    aux = np.abs(D - 1)
+    index = np.where(aux == aux.min())[0]
+
+    if aux[index] > 10e-3:
+        raise ValueError("Cannot find eigenvalue of 1. Minimum eigenvalue " +
+                         "value is {0}. Tolerance was ".format(aux[index]+1) +
+                         "set at 10e-3.")
+
+    w = V[:, index].T
+    w = w / np.sum(w)
+
+    W = np.real(np.repeat(w, n, 0))
+    I = np.eye(n)
+
+    Z = np.linalg.inv(I - P + W)
+
+    mfpt = (np.repeat(np.atleast_2d(np.diag(Z)), n, 0) - Z) / W
+
+    return mfpt

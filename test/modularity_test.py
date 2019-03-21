115606c642a75447dcf56c1b572c5f226bd8d2b6
Chris Barnes <barnesc@janelia.hhmi.org>
2019-03-21 18:25:22 -0400

    Fix bug in modularity_dir and _und
    Resolves #71
    
    Numpy.squeeze is over-eager and will reduce arrays down to ndim=0 if
    possible. This causes bugs when that "array" is indexed.

--- test/modularity_test.py
+++ test/modularity_test.py
@@ -1,4 +1,7 @@
-from .load_samples import load_sample, load_directed_sample, load_signed_sample, load_directed_low_modularity_sample
+import os
+from .load_samples import (
+    load_sample, load_directed_sample, load_signed_sample, load_directed_low_modularity_sample, TEST_DIR
+)
 import numpy as np
 import bct
 
@@ -197,3 +200,11 @@ def test_community_louvain():
     ci, q = bct.community_louvain(x, seed=seed)
     print(q)
     assert np.allclose(q, 0.2583, atol=0.015)
+
+
+def test_modularity_dir_bug71():
+    """Regression test for bug described in issue #71"""
+    fpath = os.path.join(TEST_DIR, "failing_cases", "modularity_dir_example.csv")
+    x = np.loadtxt(fpath, int, delimiter=',')
+
+    bct.modularity_dir(x)

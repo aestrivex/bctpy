01cf8e112164514806fe77cbefe3fbd07e83061e
Chris Barnes <barnesc@janelia.hhmi.org>
2019-07-23 16:35:28 -0400

    project-wide citations

--- bct/__init__.py
+++ bct/__init__.py
@@ -1,6 +1,12 @@
-__version__ = '0.5.0'
-__version_info__ = tuple(int(i) for i in __version__.split('.'))
-
 from .algorithms import *
 from .utils import *
 from .nbs import *
+from .version import __version__, __version_info__
+from .citations import BCTPY, RUBINOV2010
+
+from .due import due, BibTeX
+
+__citation__ = BCTPY
+
+due.cite(BibTeX(__citation__), description="Brain Connectivity Toolbox for Python", path="bctpy")
+due.cite(BibTeX(RUBINOV2010), description="Brain Connectivity Toolbox", path="bctpy")
